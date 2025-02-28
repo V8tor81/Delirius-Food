f185d7b4c80d7693cf6443baba15df5c2bb9bc9a
kezlima <133782241+kezlima@users.noreply.github.com>
2025-02-28 13:02:54 -0300

    teste

--- app.py
+++ app.py
@@ -4,7 +4,34 @@ from flask import request
 
 
 app = Flask(__name__)
-
-@app.route('/pedidos')
+@app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+@app.route('/pedidos')
+def pedido():
+    return render_template('pedido.html')
+
+
+@app.route('/realizar_pedido', methods=['POST'])
+def realizar_pedido():
+    metodo_pagamento = request.form['pagamento']  # Obtém o método de pagamento selecionado
+    
+    # Redireciona com base no método de pagamento escolhido
+    if metodo_pagamento == 'cartao':
+        return render_template('pagamento_cartao')
+    elif metodo_pagamento == 'pix':
+        return render_template('pagamento_pix')
+    elif metodo_pagamento == 'dinheiro':
+        return render_template('pagamento_dinheiro')
+
+@app.route('/pagamento/cartao')
+def pagamento_cartao():
+    return "Página de pagamento com Cartão"
+
+@app.route('/pagamento/pix')
+def pagamento_pix():
+    return "Página de pagamento via Pix"
+
+@app.route('/pagamento/dinheiro')
+def pagamento_dinheiro():
+    return "Página de pagamento em Dinheiro"
