afac8dc859b23682952c57aca66add6490323ef2
kezlima <133782241+kezlima@users.noreply.github.com>
2025-03-04 15:54:25 -0300

    ajustes

--- crud.py
+++ crud.py
@@ -2,19 +2,9 @@ from mysql.connector import (connection)
 
 import json
 
-
-
-'''
-####################################################################
-# Código Python para interação com o banco de dados
-####################################################################
-'''
-# Desc: Conexão com o banco de dados a partir da leitura do arquivo database/clinica.conf
-# Entrada: None
-# Saída: Conexão do banco de dados
 def conexaoBD():
     try:
-        with open('database-professor/ifood.conf', 'r') as dadosbd: 
+        with open('database-professor/ifood.conf') as dadosbd: 
             databd = json.load(dadosbd)
 
         cnx = connection.MySQLConnection(user=databd['user'],
@@ -27,17 +17,9 @@ def conexaoBD():
     except Exception as e:
         print (f"FALHA NA CONEXÃO COM O BANCO: {e}")
     
-    
-
-
-# Desc: Função responsável pelas transações de inserção, atualização e remoção no banco de dados
-# Entrada:  1 - STRING com o comando SQL desejado;
-#           2 - Tupla com os dados
-#           3 - String informando qual o tipo de comando, se INSERT, UPDATE ou DELETE
-# Saída: True se tudo ocorreu bem | False se houve alguma falha
 def createUpdateDelete(sql:str, tupla:tuple, tipo:str):
     try:
-        with open('database-professor/ifood.conf', 'r') as dadosbd: 
+        with open('database-professor/ifood.conf') as dadosbd: 
             databd = json.load(dadosbd)
 
         cnx = connection.MySQLConnection(user=databd['user'],
