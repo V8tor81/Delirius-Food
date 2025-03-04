fd4df21e0a591d3f47c4da9627dbbd89fcddc72e
kezlima <133782241+kezlima@users.noreply.github.com>
2025-03-04 18:25:27 -0300

    .

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from crud import createUpdateDelete
+from flask import session
 
 from datetime import datetime
 
