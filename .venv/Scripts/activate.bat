1f2e38b61398b4ba792a156eb847b9a9beab7590
Maria Vitória <132756495+V8tor81@users.noreply.github.com>
2025-03-07 16:05:44 -0300

    Atualizações da parte do carrinho

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\20221174010036\Documents\GitHub\Delirius-Food\.venv
+set VIRTUAL_ENV=D:\Users\20221174010014\Documents\GitHub\Delirius-Food\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from crud import createUpdateDelete
+from flask import session
 
+from datetime import datetime
+
+data_hora_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def finalizacaoPedido():
+    return render_template('finalizacaoPedido.html')
+
+
+@app.route('/finalizar_pedido', methods=['POST'])
+def adicionandoPedidoBD():
+        id = request.form['id']
+        endereco = request.form['endereco']
+        tipoPagamento = request.form['pagamento']
+
+        
+        status = createUpdateDelete(
+            "INSERT INTO pedido (id,localidade,taxaEntrega, statusPedido, tipoPagamento, dataHora, statusPagamento, idUsuarioPede) VALUES (%s, %s, 5.2, 'Aguardando Aprovação',  %s, %s, 'Pendente', 1)",
+            (id,endereco, tipoPagamento, data_hora_atual),
+            'INSERT'
+        )
+
+       
+        if tipoPagamento == 'pix':
+          return render_template('pix.html')
+      
+        
+        else:
+            return render_template('resposta.html')
+        
