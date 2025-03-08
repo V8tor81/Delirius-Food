8ab5bcfc58505cf1e442ebb333e442a646123116
kezlima <133782241+kezlima@users.noreply.github.com>
2025-03-08 17:42:12 -0300

    teste de aplicacoes flask

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import render_template
 from flask import request
 from crud import createUpdateDelete
 from flask import session
-
+from crud import read
 from datetime import datetime
+from mysql.connector import (connection)
+from crud import conexaoBD
+
+import json
+
 
 data_hora_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
@@ -12,7 +17,76 @@ app = Flask(__name__)
 
 @app.route('/')
 def finalizacaoPedido():
-    return render_template('finalizacaoPedido.html')
+    try:
+        with open('database-professor/ifood.conf', 'r') as dadosbd: 
+            databd = json.load(dadosbd)
+
+        cnx = connection.MySQLConnection(user=databd['user'],
+                                    password=databd['pass'],
+                                    host=databd['host'],
+                                    database=databd['database'])
+        cursor=cnx.cursor()
+        querry="select id, nome, preco, imagem from produto"
+        cursor.execute(querry,)
+
+        resul=cursor.fetchall()
+        cursor.close()
+    
+        return render_template('restaurantes copy.html', restaurante=resul)
+    
+    except Exception as e:
+        print (f"FALHA NA CONEXÃO COM O BANCO: {e}")
+    
+
+@app.route('/adicionando_carrinho', methods=['POST'])
+def  carrinho():
+    produto_id = request.form['produto_id']
+
+    try:
+        with open('database-professor/ifood.conf', 'r') as dadosbd: 
+            databd = json.load(dadosbd)
+
+        cnx = connection.MySQLConnection(user=databd['user'],
+                                    password=databd['pass'],
+                                    host=databd['host'],
+                                    database=databd['database'])
+        cursor=cnx.cursor()
+        querry="select id, nome, preco, imagem from produto where id=%s"
+        cursor.execute(querry,(produto_id,))
+
+        resul=cursor.fetchall()
+        cursor.close()
+        return render_template('compras2.html', carrinho=resul)
+
+    except Exception as e:
+        print (f"FALHA NA CONEXÃO COM O BANCO: {e}")
+  
+@app.route('/adicionar_ao_carrinho', methods=['POST'])
+def adicionar_ao_carrinho():
+    data = request.get_json()
+    
+    produtos = data['produtos']
+
+    # Conecta ao banco de dados
+    
+
+    # Itera sobre os produtos e insere no banco
+    for produto in produtos:
+        idProduto = produto['id']
+
+        preco = produto['preco']
+        quantidade = produto['quantidade']
+
+        # Consulta SQL para inserir no banco de dados
+    status = createUpdateDelete(
+                "insert into relacContem  idProduto = %s, preco = %s, quantidade=%s WHERE idPedido = 1",
+                (idProduto, preco, quantidade,),
+                'UPDATE'
+            ) #alteracao 4 
+    return
+
+    # Commit para garantir que os dados foram salvos
+  
 
 
 @app.route('/finalizar_pedido', methods=['POST'])
@@ -23,7 +97,7 @@ def adicionandoPedidoBD():
 
         
         status = createUpdateDelete(
-            "INSERT INTO pedido (id,localidade,taxaEntrega, statusPedido, tipoPagamento, dataHora, statusPagamento, idUsuarioPede) VALUES (%s, %s, 5.2, 'Aguardando Aprovação',  %s, %s, 'Pendente', 1)",
+            "INSERT INTO pedido (id,localidade,taxaEntrega, statusPedido, tipoPagamento, dataHora, statusPagamento, idUsuarioPede) VALUES (%s, %s, 0.0, 'Aguardando Aprovação',  %s, %s, 'Pendente', 1)",
             (id,endereco, tipoPagamento, data_hora_atual),
             'INSERT'
         )
@@ -36,3 +110,39 @@ def adicionandoPedidoBD():
         else:
             return render_template('resposta.html')
         
+####################################################################################
+@app.route('/finalizar_compra', methods=['POST'])
+def finalizar_compra():
+    data = request.get_json()
+
+    # Coletando os dados do corpo da requisição
+    cpf = data['cpf']
+    endereco = data['endereco']
+    metodo_pagamento = data['metodoPagamento']
+    produtos = data['produtos']
+  
+    # Inserir dados do pedido na tabela 'pedido'
+    status = createUpdateDelete(
+            "INSERT INTO pedido (id,localidade,taxaEntrega, statusPedido, tipoPagamento, dataHora, statusPagamento, idUsuarioPede) VALUES (%s, %s, 0.0, 'Aguardando Aprovação',  %s, %s, 'Pendente', 1)",
+            (cpf,endereco, metodo_pagamento, data_hora_atual),
+            'INSERT'
+        )
+
+    # Pegar o ID do pedido recém-criado
+
+    # Inserir itens no pedido na tabela 'item_pedido'
+    for produto in produtos:
+        id_produto = produto['idProduto']  # Agora estamos pegando o idProduto
+        nome = produto['nome']
+        preco = produto['preco']
+        quantidade = produto['quantidade']
+        total = produto['total']
+
+    status = createUpdateDelete(
+            "INSERT INTO relacContem (idProduto, idPedido,preco, quantidade) VALUES (%s, %s, %s, %s)",
+            (id_produto,cpf, preco, quantidade),
+            'INSERT'
+        )
+    
+    return 'deu certo'
+
